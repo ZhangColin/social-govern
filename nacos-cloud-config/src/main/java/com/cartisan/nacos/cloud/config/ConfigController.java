9b37d864cc06189381be9ec9001c3b351f99557f
张锦华 <colin.zhang@zsgcm.com>
2020-12-23 11:41:26 +0800

    nacos-cloud-config demo

---
+++ nacos-cloud-config/src/main/java/com/cartisan/nacos/cloud/config/ConfigController.java
@@ -0,0 +1,23 @@
+package com.cartisan.nacos.cloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author colin
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @GetMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
---
+++ nacos-cloud-config/src/main/java/com/cartisan/nacos/cloud/config/NacosCloudConfigApplication.java
@@ -0,0 +1,14 @@
+package com.cartisan.nacos.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author colin
+ */
+@SpringBootApplication
+public class NacosCloudConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosCloudConfigApplication.class, args);
+    }
+}
